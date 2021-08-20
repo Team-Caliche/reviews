9a65dc4f02e7ef7f91f916642b8d38e6615bfd21
Francisco <fveranicola@gmail.com>
2021-08-20 11:35:17 -0400

    fixing host

--- Database/db.js
+++ Database/db.js
@@ -4,9 +4,10 @@ const pool = new Pool({
   user: 'postgres',
   password: 'password',
   database: 'sdcreview',
-  host: ' 18.216.158.248',
+  host: 'https://18.216.158.248/',
   port: '5432'
 });
+
 
 
 pool.connect();
