f6fbbeb97f50af61cff3bb8ceea45b53be114696
Francisco <fveranicola@gmail.com>
2021-08-20 14:23:14 -0400

    fixing loader route again

--- Server/app.js
+++ Server/app.js
@@ -100,7 +100,7 @@ app.put('/api/reviews/:review_id/:type', (req, res) => {
 
 });
 
-app.get('/loaderio-c0341c98129bb5ebfe772fc641c829c7.txt', (req, res) => {
+app.get('/loaderio-c0341c98129bb5ebfe772fc641c829c7', (req, res) => {
     res.status(200).send('loaderio-c0341c98129bb5ebfe772fc641c829c7')
 });
 
